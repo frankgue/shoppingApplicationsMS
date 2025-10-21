80542921baa8e837e05461c7e359107e18747149
Frank GUEKENG - GKFC <frankguekeng11@gmail.com>
2025-10-21 22:42:48 +0100

    Configure the Discovery serve app

---
+++ discovery-server-service/src/main/java/com/gkfcsolution/discoveryserverservice/config/SecurityConfig.java
@@ -0,0 +1,81 @@
+package com.gkfcsolution.discoveryserverservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created on 2025 at 21:55
+ * File: null.java
+ * Project: shoppingApplicationsMS
+ *
+ * @author Frank GUEKENG
+ * @date 21/10/2025
+ * @time 21:55
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${eureka.username}")
+    private String username;
+    @Value("${eureka.password}")
+    private String password;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager configureAuthentication() {
+        List<UserDetails> userDetails = new ArrayList<>();
+        List<GrantedAuthority> employeeRoles = new ArrayList<>();
+        List<GrantedAuthority> adminRoles = new ArrayList<>();
+        List<GrantedAuthority> userRoles = new ArrayList<>();
+        employeeRoles.add(new SimpleGrantedAuthority("EMPLOYEE"));
+        adminRoles.add(new SimpleGrantedAuthority("ADMIN"));
+        userRoles.add(new SimpleGrantedAuthority("USER"));
+
+        userDetails.add(new User(username, password, employeeRoles));
+//        userDetails.add(new User("admin", passwordEncoder().encode("123456"), adminRoles));
+//        userDetails.add(new User("user", passwordEncoder().encode("123456"), userRoles));
+
+        return new InMemoryUserDetailsManager();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+
+}
